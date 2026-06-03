5a355fd79648c65b445e74cbbb3473ccd08bd765
PBrdng <pbrdng@gmail.com>
2026-06-03 10:13:40 +0200

    fix broken test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using Test
     f = System([f_1; f_2])
 
     R0 = regions(f)
-    @test ncritical_complex(R0) isa Vector
+    @test ncritical_complex(R0) isa Number
     @test nregions(R0) == 3
     @test nbounded(R0) == 0
     @test nunbounded(R0) == 0
